782969933498c7fe30942f053853fd051da0d059
Sam .M <you@example.com>
2023-08-01 20:02:45 +0300

    added routers for login and sign in

--- src/context/Contexts.jsx
+++ src/context/Contexts.jsx
@@ -1,20 +1,86 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 const Appcontext = createContext();
 function Contexts({ children }) {
-  const [cartState, setCartState] = useState({ transform: "translate(150%)" })
-  const [isClosed, setIsClosed] = useState(true)
+  const [cartState, setCartState] = useState({ transform: "translate(150%)" });
+  const [isClosed, setIsClosed] = useState(true);
+  /*   const [itemsData, setItemsData] = useState([]);
+   */ const [userState, setUserState] = useState(null);
+  const [users, setUsers] = useState([
+    {
+      first_name: "string",
+      last_name: "string",
+      email: "string",
+      is_active: true,
+      created: "2019-08-24T14:15:22Z",
+      updated: "2019-08-24T14:15:22Z",
+    },
+    {
+      first_name: "string2",
+      last_name: "string2",
+      email: "string2",
+      is_active: true,
+      created: "2019-08-24T14:15:22Z",
+      updated: "2019-08-24T14:15:22Z",
+    },
+  ]);
+  const URL = "";
   const values = {
     cartPos: cartState,
     toggleCart: handleCartPos,
     cartStatus: isClosed,
+    formFunction: handleForms,
+    userIsLoged: userState,
+  };
+
+  /* Check user state */
+  function findUser(userEmail) {
+    // Assuming you have an array of users
+
+    const foundUser = users.filter((usr) => usr.email === userEmail);
+    return foundUser;
   }
+  if (localStorage.getItem("user") !== null) {
+    const SU = localStorage.getItem("user");
+    const storedUser = JSON.parse(SU);
+    const userEmail = storedUser.email; // Replace "s" with the email you want to search for
+    const foundUser = findUser(userEmail);
+
+    useEffect(() => {
+      if (foundUser !== []) {
+        setUserState(storedUser.first_name);
+      }
+    }, []);
+  }
+  /* fetch data from api */
+  function handleForms(data) {
+    setIsSubmited((prev) => !prev);
+    fetch(URL, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+      .then((res) => {
+        if (!res.ok) {
+          alert("Failed");
+        }
+        return res.json();
+      })
+      .then((data) => data)
+      .catch((error) => console.log(error));
+  }
+  /* Check location */
+
   function handleCartPos(q) {
-    setIsClosed(prev => !prev)
+    setIsClosed((prev) => !prev);
     if (q) {
-      setCartState({ transform: "translate(0%)" })
+      setCartState({ transform: "translate(0%)" });
     } else {
-      setCartState({ transform: "translate(150%)" })
+      setCartState({ transform: "translate(150%)" });
     }
   }
   return <Appcontext.Provider value={values}>{children}</Appcontext.Provider>;
