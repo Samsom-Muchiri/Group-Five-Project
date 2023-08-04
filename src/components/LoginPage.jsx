0b2be5d2b2204cf620655b84be4d774395c2b766
alikiminza <ali.kiminza@student.moringaschool.com>
2023-08-04 11:55:42 +0300

    resolving merge conflicts

--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+<<<<<<< HEAD
 import "../style sheets/loging.css";
 
 function LoginPage() {
@@ -64,3 +65,47 @@ function LoginPage() {
 }
 
 export default LoginPage;
+=======
+
+function LoginPage(){
+    const[formData, setFormData] = useState({});
+    const[inputState, setInputState] = useState("");
+    const[pwdState, setPwdState] = useState("");
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        const LogData = {
+            username:inputState,
+            password: pwdState,
+        }
+
+    }
+    function handleInput(e) {
+        const value = e.target.value;
+        setInputState(value);
+    }    
+    function handlePwd(e) {
+        const value = e.target.value;
+        setPwdState(value);
+    }
+
+    const login = (
+        <form onSubmit={{handleSubmit}}>
+            <h1>Log in to Green Space</h1>
+            <div className="input-container">
+                <input type="text" placeholder="email" onChange={handleInput}/>
+                <br/>
+                <input type="password" placeholder="password" onChange={handlePwd}/>
+            </div>
+            <center>
+            <button className="btn">LOG IN</button>
+            </center>
+        </form>
+
+    );
+    return
+    <div className="form-wrapper">{login}</div>
+}
+
+export default LoginPage;
+>>>>>>> 6057b40 (Login Page)
